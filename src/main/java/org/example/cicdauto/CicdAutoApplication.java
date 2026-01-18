97854efe760ea2d0497798ff2ded9d7ee7b8af6d
Viva <happybird00@naver.com>
2026-01-18 13:48:19 +0900

    템플릿 테스트

--- src/main/java/org/example/cicdauto/CicdAutoApplication.java
+++ src/main/java/org/example/cicdauto/CicdAutoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CicdAutoApplication {
 
+    // h2 test
     public static void main(String[] args) {
         SpringApplication.run(CicdAutoApplication.class, args);
     }
