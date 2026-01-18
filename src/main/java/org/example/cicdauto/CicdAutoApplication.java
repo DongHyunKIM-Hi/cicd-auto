2c600a2a70d355b2fbf2019b8c7b170459849ce3
Viva <happybird00@naver.com>
2026-01-18 13:43:54 +0900

    Initial commit

---
+++ src/main/java/org/example/cicdauto/CicdAutoApplication.java
@@ -0,0 +1,13 @@
+package org.example.cicdauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdAutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdAutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cicdauto/CicdAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cicdauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdAutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
